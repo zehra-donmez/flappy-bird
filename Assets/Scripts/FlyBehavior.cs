4d8e1311483f2b0484121911b8f68eee945602e7
Dönmez <104215576+zehra-donmez@users.noreply.github.com>
2023-08-09 14:39:03 +0300

    first commit

---
+++ Assets/Scripts/FlyBehavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyBehavior : MonoBehaviour
+{
+    [SerializeField] private float _velocity=1.5f;
+    [SerializeField] private float _rotationSpeed=10f;
+
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb=GetComponent<Rigidbody2D>();    
+    }
+
+    private void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rb.velocity=Vector2.up*_velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rb.velocity.y * _rotationSpeed);
+    }
+}
---
+++ Assets/Scripts/MovePipe.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePipe : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0.65f;
+
+    public void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.left * _speed * Time.deltaTime;
+    }
+}
